70470105dc38f8d9d06cfa6d48ef4bd6a9ec64c8
frimoldi.eth <francisco.rimoldi@gmail.com>
2024-04-15 19:56:49 -0300

    Delete accounts-v2 directory

--- accounts-v2/test/utils/fixtures/uniswap-v3/extensions/UniswapV3PoolExtension.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity =0.7.6;
-
-import { UniswapV3Pool } from "@uniswap/v3-core/contracts/UniswapV3Pool.sol";
-
-contract UniswapV3PoolExtension is UniswapV3Pool {
-    constructor() UniswapV3Pool() { }
-}
